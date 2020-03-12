34f1deefcd8abf20edc37f452d75306dac91f453
MALNWTI <MALNWTI1995@gmail.com>
2020-03-12 22:48:50 +0200

    Add JS

--- js/common.js
+++ js/common.js
@@ -1,3 +1,43 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    })
+    
+    changeActive(blockID)
+    closeMenu()
+  })
+}
+
+function closeMenu() {
+    const burger = document.getElementById("IBurger");
+    const menu = document.getElementById("menu");
+    const Wburger = document.getElementById('WBurger');
+
+    burger.className = 'burgerA';
+    menu.className = 'menu';   
+    Wburger.className = 'wrapBurger wrapBurgerA'
+}
+
+function changeActive(currentLi) {
+    const anchors = document.querySelectorAll('a[href*="#"]')
+    for (let anchor of anchors) {
+        if (anchor.getAttribute('href').substr(1) == currentLi) {
+            console.log( anchor )
+            anchor.className = 'active'
+        }
+        else{
+            anchor.className = ''
+        }
+    }
+}
+
 function burger() {
     const burger = document.getElementById("IBurger");
     const menu = document.getElementById("menu");
@@ -13,3 +53,5 @@ function burger() {
         Wburger.className = 'wrapBurger wrapBurgerA'
     }
 }
+
+
